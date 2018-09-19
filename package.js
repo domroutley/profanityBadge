44fc24002b76d7bd4bcac47d57855d74193b1de1
domroutley <dominic.routley@gmail.com>
2018-09-19 22:46:47 +0100

    added package file

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "profanityBadge",
+  "description": "",
+  "author": "Dom Routley",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/domroutley/profanityBadge"
+  },
+  "license": "MIT",
+  "dependencies": {}
+}
